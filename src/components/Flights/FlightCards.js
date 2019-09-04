45d0b06132b71c0547acb91e67b2e213892e446c
Nadire Ghalip <47196130+NadireGh@users.noreply.github.com>
2019-09-03 20:18:16 -0700

    new table added

--- src/components/Flights/FlightCards.js
+++ src/components/Flights/FlightCards.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card, Button, CardTitle, CardText, Row, Col } from "reactstrap";
 import data from "../Data/flights.json";
+import { Table } from "reactstrap";
+
 const FlightCards = props => {
   console.log("data here", data);
   return (
@@ -12,8 +14,39 @@ const FlightCards = props => {
               <CardTitle>
                 {each.agent} <h2>{each.price.replace("Â", "")}</h2>
               </CardTitle>
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Larry</td>
+                    <td>the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </Table>
+
               <CardText>text he re £ content.</CardText>
-              <Button>Go somewhere</Button>
+              <Button color="success">Book Now</Button>
             </Card>
           </Col>
         );
